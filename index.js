52e712bffca0c2c92bd551a39a70db810c72dbec
leonardodamasceno8 <leonardodamasceno8@users>
2020-02-18 17:30:34 -0300

    Almoust done

--- index.js
+++ index.js
@@ -108,6 +108,21 @@ let c;
 // }
 
 
+// for (let linha = 0; linha <n; linha +=2){
+//     let linhaAsterisco = '';
+//     let rn=n-i;
+//     let y=i/2;
+//         for (let ri=0; ri<y; ri++){
+//             linhaAsterisco += ' ';
+//         }
+//         for(let nAsteriscos = 0; nAsteriscos < rn; nAsteriscos++){
+//         linhaAsterisco += '*';
+//         }
+//         console.log(linhaAsterisco);
+//     i-=2;
+// }
+
+
 
 
 
