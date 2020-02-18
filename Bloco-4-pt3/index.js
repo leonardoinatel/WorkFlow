c184e0bb7742282e83f81221f3f5671217a8c15c
leonardodamasceno8 <leonardodamasceno8@users>
2020-02-18 18:26:17 -0300

    Parte 2 funcoes

---
+++ Bloco-4-pt3/index.js
@@ -0,0 +1,30 @@
+// let info = {
+//     personagem: "Margarida", 
+//     origem: "Pato Donald",
+//     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//     recorrente: "Sim"
+// };
+
+// let info2 = {
+//     personagem: "Tio Patinhas",
+//     origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+//     nota: "O último MacPatinhas",
+//     recorrente: "Sim"
+// };
+
+// console.log("Bem vindo(a), ",info.personagem)
+// console.log(info.recorrente)
+
+// console.log("")
+// for(let chaves in info){
+//     console.log(chaves)
+// }
+
+// console.log("")
+// for(let chaves in info2){
+//     if(info[chaves] === info2[chaves]){
+//         console.log("Ambas recorrentes")
+//     }else{
+//         console.log(info[chaves], " e ", info2[chaves])
+//     }
+// }
