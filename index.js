ca1cea5734aa71411e720f6d67c7ff39dea0d773
leonardodamasceno8 <leonardodamasceno8@users>
2020-02-18 01:01:05 -0300

    Exercicio numero 2 executado

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 let a;
 let b;
 
-let soma = (a,b) => {
-    console.log(a+b)
-}
+// let soma = (a,b) => {
+//     console.log(a+b)
+// }
 
-let subtracao = (a,b) => {
-    console.log(a-b)
-}
+// let subtracao = (a,b) => {
+//     console.log(a-b)
+// }
 
-let divisao = (a,b) => {
-    if (a > b) {    
-            console.log(a/b)
+// let divisao = (a,b) => {
+//     if (a > b) {    
+//             console.log(a/b)
+//     }
+//     else {
+//             console.log(b/a)
+//         }
+// }
+
+// let multiplicacao = (a,b) => {
+//     console.log(a*b)
+// }
+
+// let modulo = (a,b) => {
+//     console.log(a%b)
+// }
+
+// soma(10,2)
+// subtracao(8,5)
+// divisao(30,5)
+// multiplicacao(60,2)
+// modulo(5,2)
+
+//exercicio numero 2
+
+
+let maior = (a,b) => {
+    if(a>b){
+        console.log("Esse e o maior numero: " + a)
+    }else{
+        console.log("Esse e o maior numero: " + b)
     }
-    else {
-            console.log(b/a)
-        }
 }
 
-let multiplicacao = (a,b) => {
-    console.log(a*b)
-}
-
-soma(10,2)
-subtracao(8,5)
-divisao(30,5)
-multiplicacao(60,2)
+maior (30,1)
