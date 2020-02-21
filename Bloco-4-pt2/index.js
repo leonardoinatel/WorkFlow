dbcdeb9a55f25e16f82586996c16aee557ac530b
Leonardo <leonardodamasceno@gec.inatel.br>
2020-02-21 04:03:57 -0300

    quebrando a cabeca com o primeio exe

---
+++ Bloco-4-pt2/index.js
@@ -0,0 +1,10 @@
+let squadAst = (n) => {
+    let asterix = [];
+    for(let i=0 ; i<n ; i++){
+        for(let j = 0 ; j < 5; j++){
+            return asterix[i][j] = "*"
+        }
+    }
+}
+
+console.log(squadAst(5))
