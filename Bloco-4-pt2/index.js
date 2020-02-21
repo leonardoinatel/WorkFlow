7aca499bd2bcc838f33ad5c26c1c123d6f6aa611
Leonardo <leonardodamasceno@gec.inatel.br>
2020-02-21 04:51:34 -0300

    Exercicio numero 2, done!

--- Bloco-4-pt2/index.js
+++ Bloco-4-pt2/index.js
@@ -1,12 +1,23 @@
-let squadAst = (n) => {
-    let asterix="";
-    for(let i=0 ; i<n ; i++){
-        asterix='';
-        for(let j = 0 ; j < n; j++){
-            asterix = asterix + "*"
-        }
-        console.log(asterix)
+//Exercicio Numero 1
+// let squadAst = (n) => {
+//     let asterix="";
+//     for(let i=0 ; i<n ; i++){
+//         asterix='';
+//         for(let j = 0 ; j < n; j++){
+//             asterix = asterix + "*"
+//         }
+//         console.log(asterix)
+//     }
+// }
+// squadAst(5)
+
+let trianguleAst = (n) =>{
+    let asterix = "";
+    for(let i = 0 ; i < n; i++){
+        asterix += "*";
+        console.log(asterix) 
     }
+
 }
 
-squadAst(5)
+trianguleAst(5)
