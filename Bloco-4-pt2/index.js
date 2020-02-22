53cbc601213b57a69b015c5bd822f25cc3eda492
Leonardo <leonardodamasceno@gec.inatel.br>
2020-02-22 00:27:07 -0300

    Programa numero primo add, done

--- Bloco-4-pt2/index.js
+++ Bloco-4-pt2/index.js
@@ -23,13 +23,26 @@
 
 // trianguleAst(5)
 
-let invertTriangule = (n) =>{
-    let asterix = {};
-    let vazio = " ";
-    for(let i = n; i>0; i--){
-        asterix[i] = asterix[i] + "*"
-        console.log(asterix[i])
+// let invertTriangule = (n) =>{
+//     let asterix = ""
+//     for(let i = 0; i<n; i++){
+//         asterix += "*"
+//     }
+//     console.log(asterix)
+// }
+// invertTriangule(5)
+
+// Faça um programa que diz se um número definido numa variável é primo ou não.
+
+// Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+
+let primoorNot = (a) => {
+    if (a%2 != 0 && a%3 != 0 && a%4 != 0 && a%5 != 0 && a%6 !=0 && a%7 != 0 && a%8 != 0 && a%9 != 0){
+        console.log("Numero "+a+" é um numero primo")
     }
-    
+    else{
+        console.log("Não é um numero primo")
+    }
 }
-invertTriangule(5)
+
+primoorNot(23)
