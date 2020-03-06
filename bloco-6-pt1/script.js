dd6ad578be422b4348c0cb21763dae3e71558e21
Leonardo <leonardodamasceno@gec.inatel.br>
2020-03-06 05:34:05 -0300

    execicio 1

---
+++ bloco-6-pt1/script.js
@@ -0,0 +1,30 @@
+// function checarEmail(){
+//     if( document.forms[0].email.value=="" 
+//        || document.forms[0].email.value.indexOf('@')==-1 
+//          || document.forms[0].email.value.indexOf('.')==-1 )
+//         {
+//           alert( "Por favor, informe um E-MAIL válido!" );
+//           return false;
+//         }
+//     }
+
+let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo",
+               "Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná",
+               "Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia",
+               "Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"];
+
+let comboEstados = document.getElementsByTagName('select');
+let options = document.createElement("option");
+
+function selectCombo() {
+    for(let i = 0; i < estados.length; i++){
+        options.value = i;
+        options.text = estados[i];    
+    };
+    console.log(comboEstados)
+}
+
+// var opt0 = document.createElement("option");
+// opt0.value = "0";
+// opt0.text = "";
+// comboCidades.add(opt0, comboCidades.options[0]);
