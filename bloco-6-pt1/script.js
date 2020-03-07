73ba6790a3aeb859256c41054614eb878d804bd6
Leonardo <leonardodamasceno@gec.inatel.br>
2020-03-07 04:31:39 -0300

    concluido na forca do odio

--- bloco-6-pt1/script.js
+++ bloco-6-pt1/script.js
@@ -1,29 +1,30 @@
-// function checarEmail(){
-//     if( document.forms[0].email.value=="" 
-//        || document.forms[0].email.value.indexOf('@')==-1 
-//          || document.forms[0].email.value.indexOf('.')==-1 )
-//         {
-//           alert( "Por favor, informe um E-MAIL válido!" );
-//           return false;
-//         }
-//     }
-
 let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo",
                "Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná",
                "Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia",
                "Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"];
 
-let comboEstados = document.getElementsByTagName('select');
-
-
-function selectCombo() {
-    for(let i = 0; i < estados.length; i++){
+function selectCombo() {    
+    for(let i = 0; i < estados.length; i++){        
         let options = document.createElement("option");
         options.value = i;
         options.text = estados[i];
-        comboEstados.append(options)    
+        let comboEstados = document.getElementById('idEstado');
+
+        comboEstados.appendChild(options);        
     }
 }
+
+function radioButton(valor) {
+    let radio1 = document.getElementById("radioCasa");
+    let radio2 = document.getElementById("radioApt");
+ 
+    if(valor.value === 'radio1'){
+        radio2.checked = false;
+    }else if (valor.value === 'radio2'){
+        radio1.checked = false;
+    }    
+}
+
 
 // var opt0 = document.createElement("option");
 // opt0.value = "0";
